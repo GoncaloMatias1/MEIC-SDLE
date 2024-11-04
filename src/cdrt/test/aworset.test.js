9ab13db46c029a81150d1343ff0860a95b53c36a
JocaFerna <up202108044@up.pt>
2024-11-04 18:52:01 +0000

    Added CDRTs, lacks adaptation to the problem!

---
+++ src/cdrt/test/aworset.test.js
@@ -0,0 +1,44 @@
+// FILEPATH: /home/joca/SDLE/project/src/cdrt/test/aworset_test.js
+const AWORSET = require('../AWORSET.js');
+
+describe('AWORSET', () => {
+  let set1, set2;
+
+  beforeEach(() => {
+    set1 = new AWORSET('set1');
+    set2 = new AWORSET('set2');
+  });
+
+  it('should initialize correctly', () => {
+    expect(set1.toString()).toBe('AWORSET: ( )');
+    expect(set2.toString()).toBe('AWORSET: ( )');
+  });
+
+  it('should add elements correctly', () => {
+    set1.add('a');
+    set1.add('b');
+    expect(set1.toString()).toBe('AWORSET: ( a b )');
+  });
+
+  it('should remove elements correctly', () => {
+    set1.add('a');
+    set1.add('b');
+    set1.remove('a');
+    expect(set1.toString()).toBe('AWORSET: ( b )');
+  });
+
+  it('should join sets correctly', () => {
+    set1.add('a');
+    set1.toString();
+    set2.add('b');
+    set2.toString();
+    set1.join(set2);
+    expect(set1.toString()).toBe('AWORSET: ( a b )');
+  });
+
+  it('should check for element presence correctly', () => {
+    set1.add('a');
+    expect(set1.contains('a')).toBe(true);
+    expect(set1.contains('b')).toBe(false);
+  });
+});
